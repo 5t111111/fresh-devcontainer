314921811831431d728505989f53f7e80b6f9b01
Hirofumi Wakasugi <333180+5t111111@users.noreply.github.com>
2024-10-14 05:16:54 +0000

    update

--- routes/profile.tsx
+++ routes/profile.tsx
@@ -1,10 +1,7 @@
 import { page, PageProps } from "fresh";
 import { define } from "../utils.ts";
+import { Jsonify } from "fresh-session";
 
-// interface User extends JsonObject {
-//   id: number;
-//   name: string;
-// }
 interface User {
   id: number;
   name: string;
@@ -13,21 +10,6 @@ interface User {
 interface Props {
   user: User;
 }
-
-type Jsonify<T> = T extends string | number | boolean | null ? T
-  : T extends Array<infer U> ? Jsonify<U>[]
-  : T extends object ? { [K in keyof T]: Jsonify<T[K]> }
-  : never;
-
-/**
- * Type for all values that may be serialized to JSON
- */
-type JsonValue = string | number | boolean | null | JsonObject | JsonValue[];
-
-/**
- * Interface for objects that may be serialized to JSON
- */
-interface JsonObject extends Record<string, JsonValue> {}
 
 export const handler = define.handlers({
   GET: (ctx) => {
